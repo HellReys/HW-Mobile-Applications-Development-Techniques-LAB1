c541eb0314c65c5f5d061139600a21adc1b2d98b
HellReys <63811690+HellReys@users.noreply.github.com>
2025-09-10 10:25:55 +0300

    Button click event added

--- app/src/main/java/com/example/helloworldapps/MainActivity.java
+++ app/src/main/java/com/example/helloworldapps/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.helloworldapps;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,13 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TextView myText = findViewById(R.id.myText);
+        Button myButton = findViewById(R.id.myButton);
+
+        myButton.setOnClickListener(v -> {
+            myText.setText("You Clicked Here Wow!");
         });
     }
 }
